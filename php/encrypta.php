332c2b32f8220e6b5d1c4f980e87e054b36d23cf
Gabriel Rodrigues <jipacoding@gmail.com>
2018-12-10 07:50:30 -0400

    Set Kutter Function

--- php/encrypta.php
+++ php/encrypta.php
@@ -1,6 +1,14 @@
 <?php
 class Encrypta{
+  private $kutter;
 
+  function __construc($theKutter = ""){
+    $this->setKutter($theKutter);
+  }
+  // Set kutter function
+  public function setKutter($kutter = ""){
+    $this->kutter = $kutter;    
+  }
 }
 
 ?>
