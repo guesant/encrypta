37b9aac846161562d1f3bd3caec3d8dac437d28a
Gabriel Rodrigues <jipacoding@gmail.com>
2018-12-10 07:52:37 -0400

    Generate a Random Kutter

--- php/encrypta.php
+++ php/encrypta.php
@@ -5,9 +5,26 @@ class Encrypta{
   function __construc($theKutter = ""){
     $this->setKutter($theKutter);
   }
-  // Set kutter function
+
+  // Set the kutter
   public function setKutter($kutter = ""){
     $this->kutter = $kutter;    
+  }
+
+  // Generate a new Kutter
+  public function randKutter(){
+    $starterKut = "CZSIechLmkJT1z4y3wiAGg0Q2HdP59Xlu/vVbjYfEtBsMNoF+WK7pa6xDRn=qrU8O";
+    
+    // Split each char to one array...
+    $newKut = str_split($starterKut);  
+    // ... and suffle it
+    shuffle($newKut);
+    
+    // With randomized array-kutter, back to string-mode
+    $newKut = implode("", $newKut);
+    
+    // Return new Kutter;
+    return $newKut;
   }
 }
 
